729eec0ad5abef2189ba4846d723b35859d99c05
Ibrahim Yagiz Akbayrak <148493396+akbayrakyagiz@users.noreply.github.com>
2026-04-15 00:54:12 +0200

    feat: add PDF export with inline blob preview and download

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { initEditor } from './editor.js'
 import { renderPreview, renderPreviewDebounced } from './preview.js'
 import { initResizer } from './resizer.js'
 import { initStylePanel } from './stylePanel.js'
+import { initPdfExport } from './pdfExport.js'
 
 initTheme()
 initResizer()
 initStylePanel()
+initPdfExport()
 const initialContent = initEditor((markdown) => renderPreviewDebounced(markdown))
 renderPreview(initialContent)
