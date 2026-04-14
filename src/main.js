a06eaf7bac806a5a5b3d5e1487b689276b0d3cef
Ibrahim Yagiz Akbayrak <148493396+akbayrakyagiz@users.noreply.github.com>
2026-04-15 00:51:46 +0200

    feat: add style panel with live CSS variable updates and persistence

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import { initTheme } from './theme.js'
 import { initEditor } from './editor.js'
 import { renderPreview, renderPreviewDebounced } from './preview.js'
 import { initResizer } from './resizer.js'
+import { initStylePanel } from './stylePanel.js'
 
 initTheme()
 initResizer()
+initStylePanel()
 const initialContent = initEditor((markdown) => renderPreviewDebounced(markdown))
 renderPreview(initialContent)
