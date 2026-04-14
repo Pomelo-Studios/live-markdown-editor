9a9a959e8d26b51d702723223f6c7a2d70c53b69
Ibrahim Yagiz Akbayrak <148493396+akbayrakyagiz@users.noreply.github.com>
2026-04-15 00:51:14 +0200

    feat: add live markdown preview with syntax highlighting

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 // src/main.js
 import { initTheme } from './theme.js'
 import { initEditor } from './editor.js'
+import { renderPreview, renderPreviewDebounced } from './preview.js'
 
 initTheme()
-const initialContent = initEditor(() => {})
+const initialContent = initEditor((markdown) => renderPreviewDebounced(markdown))
+renderPreview(initialContent)
