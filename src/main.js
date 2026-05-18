dde6bf4d6a12db4ff08419c379ad81daeb30dde5
Ibrahim Yagiz Akbayrak <148493396+akbayrakyagiz@users.noreply.github.com>
2026-05-18 13:59:56 +0200

    feat: wire tabManager into main.js bootstrapper

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 // src/main.js
 import { initTheme } from './theme.js'
-import { initEditor, scrollToLine } from './editor.js'
+import { initEditor, scrollToLine, setEditorContent } from './editor.js'
 import { renderPreview, renderPreviewDebounced, setScrollToLineCallback } from './preview.js'
 import { initResizer } from './resizer.js'
 import { initStylePanel } from './stylePanel.js'
 import { initPdfExport } from './pdfExport.js'
 import { initMobileNav } from './mobileNav.js'
 import { initFormatToolbar } from './formatToolbar.js'
+import { initTabManager, getActiveTab, updateActiveContent } from './tabManager.js'
 
 initTheme()
 initResizer()
@@ -14,6 +15,20 @@ initStylePanel()
 initPdfExport()
 initMobileNav()
 setScrollToLineCallback(scrollToLine)
-const initialContent = initEditor((markdown) => renderPreviewDebounced(markdown))
-renderPreview(initialContent)
+
+// initTabManager does NOT fire onActiveTabChange during init.
+// Initial content is read synchronously via getActiveTab() below.
+initTabManager({
+  onActiveTabChange: (content) => {
+    setEditorContent(content)
+    renderPreview(content)
+  },
+})
+
+const initial = getActiveTab().content
+initEditor(initial, (value) => {
+  updateActiveContent(value)
+  renderPreviewDebounced(value)
+})
+renderPreview(initial)
 initFormatToolbar()
